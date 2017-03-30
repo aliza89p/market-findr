8c46c4bfff0b93b20164ae904842f6a2430bc6b3
Aliza Pilisuk <apilisuk@gmail.com>
2017-03-29 18:46:16 -0700

    added basic styling

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ $(document).ready(function(){
       e.preventDefault();
       $("#markets").html("");
       var zip = $("#zipcode").val();
-      console.log(zip);
       $.ajax({
         type: "GET",
         contentType: "application/json; charset=utf-8",
@@ -24,18 +23,14 @@ $(document).ready(function(){
               url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + marketId,
               dataType: 'jsonp'
             }).done(function(market){
-              $("#results").text("Results");
               var marketHeaders = $("#markets").append("<h2>" + marketName.substr(marketName.indexOf(' ')+1) + "</h2>");
-              if(market.marketdetails.Address.length !== 0){
-                $(marketHeaders).append("<li>Address: " + market.marketdetails.Address + "</li>");
-              }
+              $(marketHeaders).append('<li><strong>Address</strong>: <a href="' + market.marketdetails.GoogleLink + '" target="_blank">' + market.marketdetails.Address + '</a></li>');
               if(market.marketdetails.Products.length !== 0){
-                $(marketHeaders).append("<li>Products: " + market.marketdetails.Products + "</li>");
+                $(marketHeaders).append("<li><strong>Products</strong>: " + market.marketdetails.Products + "</li>");
               }
-              if(market.marketdetails.Schedule.length < 1){
-                $(marketHeaders).append("<li>Open: " + market.marketdetails.Schedule + "</li>");
+              if(market.marketdetails.Schedule.includes("to")){
+                $(marketHeaders).append("<li><strong>Open</strong>: " + market.marketdetails.Schedule + "</li>");
               }
-                $(marketHeaders).append('<li><a href="' + market.marketdetails.GoogleLink + '" target="_blank">Open in Google Maps</a></li><br /><br />');
             });
           });
         });
